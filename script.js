eca509ff5223f95c4b87e52f0167d17519e7ebf5
BobLoubar <159928577+BobLoubar@users.noreply.github.com>
2024-10-08 15:41:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
+// Initialisation de la carte centrée sur Argenteuil (48.9477, 2.2477)
+var map = L.map('map').setView([48.9477, 2.2477], 13);
 
+// Ajout des tuiles OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-<div id="map"></div>
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-    <script>
-        // Initialisation de la carte centrée sur Argenteuil (48.9477, 2.2477)
-        var map = L.map('map').setView([48.9477, 2.2477], 13);
+// Ajout d'un marqueur Outil en main
+var customIcon = L.icon({
+    iconUrl: 'https://img.icons8.com/?size=100&id=21081&format=png&color=000000', // Lien vers l'icône personnalisée
+    iconSize: [38, 38], // Taille de l'icône
+    iconAnchor: [22, 38], // Point de l'icône qui sera au point du marqueur
+    popupAnchor: [-3, -76] // Point depuis l'ancre où apparaîtra la popup
+});
 
-        // Ajout des tuiles OpenStreetMap
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+// Création du marqueur sans ouvrir la popup par défaut
+var marker = L.marker([48.94060424918679, 2.2364254535279544], { icon: customIcon }).addTo(map);
 
-        // Ajout d'un marqueur Outil en main
-        var customIcon = L.icon({
-            iconUrl: 'https://img.icons8.com/?size=100&id=21081&format=png&color=000000', // Lien vers l'icône personnalisée
-            iconSize: [38, 38], // Taille de l'icône
-            iconAnchor: [22, 38], // Point de l'icône qui sera au point du marqueur
-            popupAnchor: [-3, -76] // Point depuis l'ancre où apparaîtra la popup
-        });
+// Liaison de la popup au marqueur
+marker.bindPopup("<b>L'Outil en Main</b>");
 
-        // Création du marqueur sans ouvrir la popup par défaut
-        var marker = L.marker([48.94060424918679, 2.2364254535279544], { icon: customIcon }).addTo(map);
-
-        // Liaison de la popup au marqueur
-        marker.bindPopup("<b>L'Outil en Main</b>");
-
-        // Événement de clic pour ouvrir la popup
-        marker.on('click', function(e) {
-            this.openPopup();
-        });
-    </script>
-</body>
-</html>
+// Événement de clic pour ouvrir la popup
+marker.on('click', function(e) {
+    this.openPopup();
+});
