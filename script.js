d8e675d85daa51c7102474b6f84e5552a7a39968
BobLoubar <159928577+BobLoubar@users.noreply.github.com>
2024-10-17 10:21:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,79 +13,41 @@ var customIcon = L.icon({
     iconAnchor: [12.5, 12.5], // Point de l'icône qui sera au point du marqueur
     popupAnchor: [-4, -15] // Point depuis l'ancre où apparaîtra la popup
 });
-
-// Création du marqueur sans ouvrir la popup par défaut
 var marker = L.marker([48.94060424918679, 2.2364254535279544], { icon: customIcon }).addTo(map);
-
-// Liaison de la popup au marqueur
 marker.bindPopup("<b>L'Outil en Main</b>");
-
-// Événement de clic pour ouvrir la popup
 marker.on('click', function(e) {
     this.openPopup();
 });
 
 // Marqueurs Vignes
-var customIcon = L.icon({
-    iconUrl: 'https://img.icons8.com/?size=100&id=vnwvFJpUeg6L&format=png&color=000000', // Lien vers l'icône personnalisée
-    iconSize: [25, 25], // Taille de l'icône
-    iconAnchor: [12.5, 12.5], // Point de l'icône qui sera au point du marqueur
-    popupAnchor: [-4, -15] // Point depuis l'ancre où apparaîtra la popup
+var customIconVignes = L.icon({
+    iconUrl: 'https://img.icons8.com/?size=100&id=vnwvFJpUeg6L&format=png&color=000000', 
+    iconSize: [25, 25], 
+    iconAnchor: [12.5, 12.5], 
+    popupAnchor: [-4, -15]
 });
 var lieux = [
     { coords: [48.965208, 2.221506], nom: "Les Coteaux d'Argenteuil" },
     { coords: [48.968559, 2.241287], nom: "Vin de Sannois" }
 ];
 lieux.forEach(function(lieu) {
-    var marker = L.marker(lieu.coords, { icon: customIcon }).addTo(map);
+    var marker = L.marker(lieu.coords, { icon: customIconVignes }).addTo(map);
     marker.bindPopup("<b>" + lieu.nom + "</b>");
     marker.on('click', function(e) {
         this.openPopup();
     });
 });
 
-// Fichier GeoJSON - Quartiers d'Argenteuil
-
-// Fonction pour styliser le contour
-function styleContour(feature) {
-    return {
-        color: 'red',      // Couleur du contour
-        weight: 2,         // Épaisseur de la ligne
-        opacity: 1,         // Opacité de la ligne
-        fillColor: 'none', // Pas de couleur de remplissage
-        fillOpacity: 0     // Pas de remplissage visible
-    };
-}
-// Upload du fichier geojson
-fetch('Les_quartiers.geojson')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erreur lors du chargement du fichier GeoJSON');
-        }
-        return response.json();
-    })
-    .then(data => {
-        L.geoJSON(data, {
-            style: styleContour
-        }).addTo(map);
-    })
-    .catch(error => {
-        console.error('Erreur:', error);
-    });
-
 // Fichier GeoJSON - La plaine d'Argenteuil
 function styleContour2(feature) {
     return {
-        color: 'blue',      // Couleur du contour
-        weight: 2,         // Épaisseur de la ligne
-        opacity: 1,         // Opacité de la ligne
+        color: 'blue',      
+        weight: 2,         
+        opacity: 1,         
     };
 }
 
-// Nom que tu veux afficher
-const Name_Plaine_Argenteuil = ("<b>La plaine d'Argenteuil (projet maraîcher 2026)<b>");
-
-// Upload du fichier geojson
+var plaineLayerGroup = L.layerGroup();
 fetch('La_Plaine_Argenteuil.geojson')
     .then(response => {
         if (!response.ok) {
@@ -94,30 +56,31 @@ fetch('La_Plaine_Argenteuil.geojson')
         return response.json();
     })
     .then(data => {
-        L.geoJSON(data, {
+        var plaineLayer = L.geoJSON(data, {
             style: styleContour2,
             onEachFeature: function(feature, layer) {
-                // Ajoute une popup avec le nom seulement lors du clic sur la zone
+                const Name_Plaine_Argenteuil = "<b>La plaine d'Argenteuil (projet maraîcher 2026)</b>";
                 layer.on('click', function() {
                     layer.bindPopup(Name_Plaine_Argenteuil).openPopup();
                 });
             }
-        }).addTo(map);
+        });
+        plaineLayerGroup.addLayer(plaineLayer);
     })
     .catch(error => {
         console.error('Erreur:', error);
     });
 
-
 // Fichier GeoJSON - Les PAE d'Argenteuil
 function styleContour3(feature) {
     return {
-        color: 'green',      // Couleur du contour
-        weight: 2,         // Épaisseur de la ligne
-        opacity: 1,         // Opacité de la ligne
+        color: 'green',      
+        weight: 2,         
+        opacity: 1,         
     };
 }
 
+var paeLayerGroup = L.layerGroup();
 fetch('Les_parcs_d_Activités_Economiques_à_Argenteuil.geojson')
     .then(response => {
         if (!response.ok) {
@@ -126,38 +89,31 @@ fetch('Les_parcs_d_Activités_Economiques_à_Argenteuil.geojson')
         return response.json();
     })
     .then(data => {
-        L.geoJSON(data, {
+        var paeLayer = L.geoJSON(data, {
             style: styleContour3,
             onEachFeature: function(feature, layer) {
-                // Récupère le nom de la zone depuis les propriétés du GeoJSON
-                const zoneName = feature.properties.NOM_SECTEU;  // Assurez-vous que "nom" correspond bien à la clé dans votre GeoJSON
-                
-                // Ajoute une popup avec le nom spécifique lors du clic sur la zone
+                const zoneName = feature.properties.NOM_SECTEU;
                 layer.on('click', function() {
                     layer.bindPopup(`<b>${zoneName}</b>`).openPopup();
                 });
             }
-        }).addTo(map);
+        });
+        paeLayerGroup.addLayer(paeLayer);
     })
     .catch(error => {
         console.error('Erreur:', error);
     });
 
 // Fichier GeoJSON - Les QPV d'Argenteuil
-
-// Fonction pour styliser les contours des zones
 function styleContour4(feature) {
     return {
-        color: 'yellow',      // Couleur du contour
-        weight: 2,            // Épaisseur de la ligne
-        opacity: 1,           // Opacité de la ligne
+        color: 'yellow',      
+        weight: 2,            
+        opacity: 1,           
     };
 }
 
-// Créer un groupe de couches pour les QPV
 var qpvLayerGroup = L.layerGroup();
-
-// Charger le fichier GeoJSON et ajouter les zones à la carte
 fetch('votre_fichier_wgs84.json')
     .then(response => {
         if (!response.ok) {
@@ -166,31 +122,25 @@ fetch('votre_fichier_wgs84.json')
         return response.json();
     })
     .then(data => {
-        // Ajouter les zones QPV au groupe de couches
         var qpvLayer = L.geoJSON(data, {
             style: styleContour4,
-            onEachFeature: function (feature, layer) {
-                // Récupérer le nom de la zone depuis les propriétés du GeoJSON
-                const zoneName = feature.properties.nom_quartier;  // Assurez-vous que "nom" est bien dans votre fichier GeoJSON
-
-                // Ajouter une popup avec le nom de la zone lors du clic
+            onEachFeature: function(feature, layer) {
+                const zoneName = feature.properties.nom_quartier;
                 layer.bindPopup(`<b>${zoneName}</b>`);
-
-                // Ajouter la couche au groupe de couches QPV
                 qpvLayerGroup.addLayer(layer);
             }
         });
-        
-        // Ajouter le groupe de couches à la carte
-        qpvLayerGroup.addTo(map);
+        qpvLayerGroup.addLayer(qpvLayer);
     })
     .catch(error => {
         console.error('Erreur:', error);
     });
 
-// Ajouter une légende avec un contrôle interactif
+// Ajouter une légende avec un contrôle interactif pour les différentes zones
 var overlays = {
-    "Quartiers prioritaires": qpvLayerGroup,  // Nom de la couche à afficher dans la légende
+    "La Plaine d'Argenteuil": plaineLayerGroup,
+    "Parcs d'Activités Économiques": paeLayerGroup,
+    "Quartiers prioritaires": qpvLayerGroup
 };
 
 // Ajouter le contrôle de couches à la carte (couches visibles/cachées)
